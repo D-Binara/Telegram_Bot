8a375734887f796b76ecf8645608131a02c8d494
D_Binara <dbkaveesha@gmail.com>
2023-07-02 22:11:42 +0530

    update

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 
 print('Starting up bot...')
 
-TOKEN: Final = '6149840427:AAH2D3O8qcjo-dV-S9sUL5_1W9hnq7LJSvA'
-BOT_USERNAME: Final = ' @first200win_bot'
+TOKEN: Final = ''
+BOT_USERNAME: Final = ' '
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Hello there! I\'m a bot. What\'s up?')
