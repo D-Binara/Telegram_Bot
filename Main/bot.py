7de32e4df23954f7efb6f2eac4620bb1ede06f00
D_Binara <dbkaveesha@gmail.com>
2023-09-16 23:23:10 +0530

    update

--- Main/bot.py
+++ Main/bot.py
@@ -1,6 +1,6 @@
 from typing import Final
 
-from Token import token,username  
+from Token import token, username
 
 from telegram import Update
 from telegram.ext import *
@@ -13,31 +13,42 @@ print('Starting up bot...')
 TOKEN: Final = token
 BOT_USERNAME: Final = username
 
-button1 = keyboard_button('Hello')
-button2 = keyboard_button('Test')
 
-keyboard1 = ReplyKeyboardMarkup(resize_keyboard=True).add(button1)
-keyboard2 = ReplyKeyboardMarkup
-
+# commands
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('Hello there! I\'m a bot. What\'s up?', reply_markup=keyboard1 )
+    await update.message.reply_text('Hello there! I\'m a bot. What\'s up?')
+
 
 async def about_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('I am testing bot')
+
 
 async def language_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('This is a custom command, you can add whatever text you want here.')
 
 
+# responses
+
+def handle_text_message(update: Update, context: CallbackContext):
+    user_message = update.message.text.lower()
+
+    if 'hello ' in  user_message:
+        return "Hello"
+
+    return 'I can not understand you'
+
 
 if __name__ == '__main__':
     app = Application.builder().token(TOKEN).build()
 
+    # commands
     app.add_handler(CommandHandler('start', start_command))
     app.add_handler(CommandHandler('about', about_command))
     app.add_handler(CommandHandler('language', language_command))
-    app.add_handler(CommandHandler('Hello', button1))
 
+    # Message
+    app.add_handler(MessageHandler(filters.text & ~filters.command, handle_text_message))
+
+    # polling
     print('Polling...')
-
     app.run_polling(poll_interval=2)
